ed76ce05bf73ce22395acc2c46294d257128c9ea
NaVIn69 <navinpal512002@gmail.com>
2025-12-28 17:05:40 +0530

    fix eslint for test setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,14 @@ export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs','jest.config.js',"**/*.spec.ts", "**/*.test.ts"],
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.js',
+            '**/*.spec.ts',
+            '**/*.test.ts',
+        ],
     },
     // {
     //   files: ["**/*.ts", "**/*.cts", "**/*.mts"],
